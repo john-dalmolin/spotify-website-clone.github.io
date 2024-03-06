5b22979d8f5f7eb28730ea42925f078a79936f0b
John Dalmolin <82729254+john-dalmolin@users.noreply.github.com>
2024-03-05 21:50:54 -0300

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,92 @@
-/* ao clicar no botao para ampliar para a direita, remover atributo: "OlnSvEViCZ_vVdnc3mSQ" da div 
- classe: "KjPUGV8uMbl_0bvk9ePv". E adicionar atributo: "MUloQuW1xQawwVs0mDp4" no lugar. */
+/*=============== CATEGORIES ===============*/
 
-/* ao clicar no botao para ampliar para a direita, remover atributo: "VfDGbMWaJe9rcefizTNk" da div 
-classe 1: "XTk61Y8OkBdUT6Wj4F6i" e adicionar atributo na div classe2: "XTk61Y8OkBdUT6Wj4F6i". */
+/*
+ * Troca classes e controla a exibição dos botões de seta em uma seção de categorias.
+ * @param {NodeList} sections - Lista de elementos que representam as seções de categorias.
+ * @param {Array} arrow - Array contendo os elementos dos botões de seta (direita e esquerda).
+ * @param {string} classAdicionar - Classe a ser adicionada às seções de categorias.
+ * @param {string} classRemover - Classe a ser removida das seções de categorias.
+ */
 
-/* get the elements */
-const menuCategories = document.querySelectorAll(".KjPUGV8uMbl_0bvk9ePv.OlnSvEViCZ_vVdnc3mSQ");
-const leftArrow = document.querySelectorAll(".XTk61Y8OkBdUT6Wj4F6i");
-const rightArrow = document.querySelectorAll(".XTk61Y8OkBdUT6Wj4F6i.VfDGbMWaJe9rcefizTNk");
+// Função para trocar classes e controle dos botões de seta
+function trocarClasses(sections, arrow, classAdicionar, classRemover) {
+  sections.forEach((section) => {
+    section.classList.remove(classRemover);
+    section.classList.add(classAdicionar);
+  });
 
-/* validate if constants exist */
-if (rightArrow.length > 0 && menuCategories.length === rightArrow.length) {
-  rightArrow.forEach((arrow, i) => {
-    arrow.addEventListener("click", () => {
-      // Remove a classe do botão de seta para a direita
-      arrow.classList.remove("VfDGbMWaJe9rcefizTNk");
-      // Adiciona a classe ao elemento de menu correspondente
-      menuCategories[i].classList.remove("OlnSvEViCZ_vVdnc3mSQ");
-      menuCategories[i].classList.add("MUloQuW1xQawwVs0mDp4");
-      leftArrow[i].classList.add("VfDGbMWaJe9rcefizTNk");
-    });
+  arrow.forEach((arrow) => {
+    arrow.classList.toggle("VfDGbMWaJe9rcefizTNk");
   });
 }
+
+// Elementos selecionados
+const categoriesSection = document.querySelectorAll(".KjPUGV8uMbl_0bvk9ePv"),
+  rightArrow = document.getElementById("right-arrow"),
+  leftArrow = document.getElementById("left-arrow");
+
+// Evento de clique para a seta direita
+if (rightArrow) {
+  rightArrow.addEventListener("click", () => {
+    trocarClasses(
+      categoriesSection,
+      [rightArrow, leftArrow],
+      "MUloQuW1xQawwVs0mDp4",
+      "OlnSvEViCZ_vVdnc3mSQ"
+    );
+  });
+}
+
+// Evento de clique para a seta esquerda
+if (leftArrow) {
+  leftArrow.addEventListener("click", () => {
+    trocarClasses(
+      categoriesSection,
+      [leftArrow, rightArrow],
+      "OlnSvEViCZ_vVdnc3mSQ",
+      "MUloQuW1xQawwVs0mDp4"
+    );
+  });
+}
+
+/*=============== SCROLLBAR ===============*/
+
+/*
+ * Controla a exibição das barras de rolagem horizontal e vertical em uma área de conteúdo.
+ * @param {HTMLElement} scrollbarContent - Elemento que contém o conteúdo com as barras de rolagem.
+ * @param {HTMLElement} scrollbarHorizontal - Elemento da barra de rolagem horizontal.
+ * @param {HTMLElement} scrollbarVertical - Elemento da barra de rolagem vertical.
+ * @param {number} timeoutDuration - Duração do timeout em milissegundos (opcional, padrão é 1000).
+ */
+
+function controlarBarraDeRolagem(scrollbarContent, scrollbarHorizontal, scrollbarVertical, timeoutDuration = 1000) {
+  let timeoutScrollbar;
+
+  if (scrollbarContent) {
+    // Adiciona o evento de mouseenter ao elemento
+    scrollbarContent.addEventListener("mouseenter", () => {
+      clearTimeout(timeoutScrollbar); // Limpa o timeout se o mouse entrar novamente
+      // Remove as classes
+      scrollbarHorizontal.classList.remove("os-scrollbar-auto-hidden");
+      scrollbarVertical.classList.remove("os-scrollbar-auto-hidden");
+    });
+
+    // Adiciona o evento de mouseleave ao elemento
+    scrollbarContent.addEventListener("mouseleave", () => {
+      timeoutScrollbar = setTimeout(() => {
+        // Adiciona as classes após o timeout
+        scrollbarHorizontal.classList.add("os-scrollbar-auto-hidden");
+        scrollbarVertical.classList.add("os-scrollbar-auto-hidden");
+      }, timeoutDuration);
+    });
+  }
+}
+
+// Elementos selecionados
+const scrollbarContent = document.querySelector(".os-content"),
+      scrollbarHorizontal = document.getElementById("os-scrollbar-horizontal"),
+      scrollbarVertical = document.getElementById("os-scrollbar-vertical");
+
+// Chama a função para controlar a barra de rolagem
+controlarBarraDeRolagem(scrollbarContent, scrollbarHorizontal, scrollbarVertical);
+
